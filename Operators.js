9069fe49e8314e90ce6e028a581c42199c946505
Ragavii19 <ragavij25csa@gamail.com>
2026-06-12 10:59:37 +0530

    OPERATORS

---
+++ Operators.js
@@ -0,0 +1,25 @@
+//arithemtic
+var a = 10;
+var b =10;
+console.log(a+b)
+
+var c = 10;
+var d =10;
+console.log(c-d)
+
+var e = 10;
+var d = 10;
+console.log(e*d)
+
+var f = 10;
+var g =10;
+console.log(f%g)
+
+var h = 10;
+var i =10;
+console.log(h/i) 
+
+var j = 10;
+var k =10;
+console.log(j**k)
+
