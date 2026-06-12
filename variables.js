4af4cd976c8e1706905464596e16d835add76d29
Ragavii19 <ragavij25csa@gamail.com>
2026-06-12 10:30:33 +0530

    variables example

---
+++ variables.js
@@ -0,0 +1,11 @@
+// variables declaration using var
+var name = "19"
+console.log(name)
+ 
+//variables declaration using let 
+let r = "VETIAS"
+console.log(r)
+
+//variables declaration using 
+const a = "ragavi"
+console.log(a)
