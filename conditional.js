e17671e89f89ca18e43b08d490d00dc8dcdeb257
Ragavii19 <ragavij25csa@gamail.com>
2026-06-12 16:10:40 +0530

    CONDITION

---
+++ conditional.js
@@ -0,0 +1,23 @@
+console.log("CONDITIONAL STATEMENT")
+var light = "green"
+
+if(light == "red")
+{
+    console.log("STOP")
+}
+
+ else if(light == "white")
+{
+    console.log("READY")
+}
+
+ else if(light == "green")
+ {
+    console.log("GO !!")
+ }
+
+ else {
+    console.log("INVALID LIGHT COLOUR")
+ }
+
+ console.log("outside if")
